abcf2b25b49b07255f5c8deb8ac7adc47d9e28f7
Nikolas <nikolas.tadeu2@gmail.com>
2022-05-22 00:04:10 -0300

    Configure security_group

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -6,3 +6,47 @@ resource "aws_vpc" "vpc_01" {
       Name = "vpc1"
   }
 }
+
+# Configurando Security Group
+
+resource "aws_security_group" "sg_01" {
+  name        = "sg_01"
+  description = "Networking rules"
+  vpc_id      = aws_vpc.vpc_01.id
+
+  ingress {
+    description      = "HTTPS"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+    ingress {
+    description      = "HTTP"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description      = "SSH"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "ssh"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "lab"
+  }
+}
